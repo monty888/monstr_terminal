85f75fe2fad4f355a4d21ae74f1f4a8de7260f5b
shaun <firedragon888@protonmail.com>
2023-02-19 14:31:49 +0000

    added option to disable ack/command(nip20) events to relay and store=none option.

--- run_relay.py
+++ run_relay.py
@@ -49,16 +49,19 @@ usage: python run_relay.py --host=localhost --port=8081
 --host      -   host relay will listen websocket at, default %s
 --port      -   port relay will listen websocket on, default %s
 --endpoint  -   endpoint relay will listen websocket on, default %s
--s --store  -   storage type where relay will persist events etc. either sqllite, postgres, transient or none, default %s
+-s --store  -   storage type where relay will persist events etc. either sqlite, postgres, transient or none, default %s
 --dbfile    -   when --store is sqlite the db file for the database, default:
                 %s
                 when using dir .nostrpy dir it will be created if it doesn't exist already - other dirs wont and
                 should be created manually. The dbfile will be created if it doesn't already exist.
 --maxsub    -   maximum open subs allowed per client websocket, default %s
 --maxlength -   maximum length for event content if any
---nip15     -   disable NIP15 - End Of Stored Events(EOSE) 
---nip16     -   disable NIP16 - Event treatment, ephemeral and replaceable event ranges as
+--nip15     -   disable NIP15 - End Of Stored Events(EOSE)
+                see https://github.com/nostr-protocol/nips/blob/master/15.md 
+--nip16     -   disable NIP16 - Event treatment, ephemeral and replaceable event ranges
+                see https://github.com/nostr-protocol/nips/blob/master/16.md
 --nip20     -   disable NIP20 - OK command events  
+                see https://github.com/nostr-protocol/nips/blob/master/20.md
   
     """ % (HOST, PORT, END_POINT, DB_TYPE, SQL_LITE_FILE, MAX_SUB))
 
