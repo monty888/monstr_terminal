21d27fb90556d1cbbdcc85c90e4c9cdf411514ea
monty <Monty888@protonmail.com>
2023-05-11 16:52:58 +0100

    mirror.py wrote to do basic mirroring from a relay to another

---
+++ mirror.py
@@ -0,0 +1,91 @@
+"""
+watches one relay and post what it sees into another
+"""
+import logging
+import asyncio
+from datetime import datetime,timedelta
+from monstr.util import util_funcs
+from monstr.client.client import Client, ClientPool
+from monstr.client.event_handlers import RepostEventHandler
+
+
+async def do_mirror(from_url,
+                    to_url,
+                    base_filter=None,
+                    as_users=None,
+                    for_user=None,
+                    since=datetime.now()):
+
+    def get_filter():
+        ret = base_filter
+        if ret is None:
+            ret = {
+                'since': util_funcs.date_as_ticks(datetime.now())
+            }
+
+        return ret
+
+    async with ClientPool(from_url) as from_relay:
+        print('connected to from relay')
+        use_filter = get_filter()
+
+
+        # now start mirroring to to_url
+        async with ClientPool(to_url) as to_relay:
+            print('mirroring from %s to %s with filter %s' % (from_url,
+                                                              to_url,
+                                                              use_filter))
+            my_repost = RepostEventHandler(to_relay)
+
+            def on_connect(the_from_relay):
+                the_from_relay.subscribe('my_mirror',
+                                         handlers=my_repost,
+                                         filters=use_filter)
+
+            # manually call to make the sub
+            on_connect(from_relay)
+
+            while True:
+                await asyncio.sleep(0.5)
+
+
+    # # where we're posting to
+    # to_relay = ClientPool(to_relay)
+    # asyncio.create_task(to_relay.run())
+    #
+    # # TODO add EOSE support
+    # my_repost = RepostEventHandler(to_relay)
+    # def on_connect(the_from_relay):
+    #     the_from_relay.subscribe(handlers=my_repost, filters=get_filter())
+    #
+    # from_relay = ClientPool(from_relay, on_connect=on_connect)
+    # await from_relay.run()
+    #
+    # print('starting mirror \nfrom %s to %s \nwith filter=%s' % (from_relay, to_relay, filter))
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    # from_relay = ['ws://localhost:8082/','ws://localhost:8083/']
+    from_relay = ['wss://nos.lol']
+    to_relay = ['ws://localhost:8081/']
+
+    # base filter for example to only mirror set kinds
+    # {kinds: [Event.KIND..., ]}
+    base_filter = None
+
+    # if given the kind 3 for this keys will be looked up and
+    # the base query will be modified to request from aithors
+    # mentioned in the returned contacts, default any
+    as_user = None
+
+    # base query will be modified to add this authors, default any
+    # if neither as_user or for_user are given then author wont be set which means
+    # events for everyone will be returned
+    for_user = None
+
+    # events will be fetch from this time point, default now
+    since = None
+
+    asyncio.run(do_mirror(from_relay, to_relay,
+                          base_filter=base_filter))
