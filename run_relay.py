8b0fe45fc30febd6cc2e7b6e61ae0917520e5fe8
monty <Monty888@protonmail.com>
2023-07-11 16:17:28 +0100

    relay updated to support nip33 and linked in monstr EOSE support assumed - emulated removed

--- run_relay.py
+++ run_relay.py
@@ -1,36 +1,3 @@
-"""
-usage: run_relay.py [-h] [--host HOST] [--port PORT] [--endpoint ENDPOINT] [-s {sqlite,postgres,transient,none}] [--dbfile DBFILE]
-                    [--pg_database PG_DATABASE] [--pg_user PG_USER] [--pg_password PG_PASSWORD] [--maxsub MAXSUB] [--maxlength MAXLENGTH]
-                    [--nip15] [--nip16] [--nip20] [--ssl] [--tor] [-w] [-d]
-
-runs a nostr relay
-
-options:
-  -h, --help            show this help message and exit
-  --host HOST           ip address where relay will listen, default[localhost]
-  --port PORT           port relay will listen, default[8081]
-  --endpoint ENDPOINT   endpoint address for the relay websocket[/]
-  -s {sqlite,postgres,transient,none}, --store {sqlite,postgres,transient,none}
-                        storage type to use for received events, default[sqlite]
-  --dbfile DBFILE       when store is sqlite the file location for the db, default[/.nostrpy/nostr-relay.db]
-  --pg_database PG_DATABASE
-                        when store is postgres the postgres db name, default[nostr-relay]
-  --pg_user PG_USER     when store is postgres the postgres username, default[postgres]
-  --pg_password PG_PASSWORD
-                        when store is postgres the postgres password
-  --maxsub MAXSUB       maximum open subs allowed per client websocket, default[10]
-  --maxlength MAXLENGTH
-                        maximum length for event content if any, default[None]
-  --nip15               disable NIP15 - End Of Stored Events(EOSE) see https://github.com/nostr-protocol/nips/blob/master/15.md, default[False]
-  --nip16               disable NIP16 - Event treatment, ephemeral and replaceable event ranges see https://github.com/nostr-
-                        protocol/nips/blob/master/16.md, default[False]
-  --nip20               disable NIP20 - OK command events see https://github.com/nostr-protocol/nips/blob/master/20.md, default[False]
-  --ssl                 run ssl ssl_key and ssl_cert will need to be defined
-  --tor                 make realy accessable over tor
-  -w, --wipe            wipes event store and exits
-  -d, --debug           enable debug output
-
-"""
 import logging
 import sys
 import os
@@ -46,7 +13,9 @@ except Exception as e:
     pass
 from monstr.relay.relay import Relay
 from monstr.relay.accept_handlers import LengthAcceptReqHandler
-from monstr.event.persist import RelayMemoryEventStore, ARelaySQLiteEventStore, RelayPostgresEventStore
+from monstr.event.persist import RelayPostgresEventStore # postgres store needs to be fixed...
+from monstr.event.persist_sqlite import ARelaySQLiteEventStore
+from monstr.event.persist_memory import RelayMemoryEventStore
 from monstr.util import ConfigError
 from util import load_toml
 import ssl
@@ -75,7 +44,7 @@ def create_work_dir():
         os.makedirs(WORK_DIR)
 
 
-async def get_sql_store(filename, is_nip16):
+async def get_sql_store(filename, is_nip16, is_nip33):
     f = Path(filename)
 
     parent_dir = f.parts[len(f.parts)-2]
@@ -91,7 +60,8 @@ async def get_sql_store(filename, is_nip16):
     # if it does we'll assume everything is ok...we could do more
 
     ret = ARelaySQLiteEventStore(filename,
-                                 is_nip16=is_nip16)
+                                 is_nip16=is_nip16,
+                                 is_nip33=is_nip33)
     if not ret.exists():
         logging.info(f'get_sql_store::create new db {filename}')
         await ret.create()
@@ -145,19 +115,35 @@ def get_cmdline_args(args) -> dict:
                         help=f'maximum open subs allowed per client websocket, default[{args["maxsub"]}]')
     parser.add_argument('--maxlength', action='store', default=args['maxlength'], type=int,
                         help=f'maximum length for event content if any, default[{args["maxlength"]}]')
-    # nip support flags as flag is to turn off they're output not'ed
-    parser.add_argument('--nip15', action='store_true',
-                        help=f"""disable NIP15 - End Of Stored Events(EOSE)
-                see https://github.com/nostr-protocol/nips/blob/master/15.md, default[{not args["nip15"]}]""",
-                        default=args['nip15'])
+
+    # nip support flags, both enable and disable versions so a def true can be overriden
     parser.add_argument('--nip16', action='store_true',
-                        help=f"""disable NIP16 - Event treatment, ephemeral and replaceable event ranges
-                see https://github.com/nostr-protocol/nips/blob/master/16.md, default[{not args["nip16"]}]""",
+                        help=f"""enable NIP16 - Event treatment, ephemeral and replaceable event ranges
+                see https://github.com/nostr-protocol/nips/blob/master/16.md, default[{args["nip16"]}]""",
                         default=args['nip16'])
+
+    parser.add_argument('--no-nip16', action='store_false',dest='nip16',
+                        help=f"""disable NIP16 - Event treatment, ephemeral and replaceable event ranges
+                        default[{not args["nip16"]}]""",
+                        default=args['nip16'])
+
     parser.add_argument('--nip20', action='store_true',
-                        help=f"""disable NIP20 - OK command events  
-                see https://github.com/nostr-protocol/nips/blob/master/20.md, default[{not args["nip20"]}]""",
+                        help=f"""enable NIP20 - OK command events  
+                see https://github.com/nostr-protocol/nips/blob/master/20.md, default[{args["nip20"]}]""",
                         default=args['nip20'])
+    parser.add_argument('--no-nip20', action='store_false',dest='nip20',
+                        help=f"""disable NIP20 - OK command events, default[{not args["nip20"]}]""",
+                        default=args['nip20'])
+
+    parser.add_argument('--nip33', action='store_true',
+                        help=f"""disable NIP33 - Parameterized Replaceable Events
+                    see https://github.com/nostr-protocol/nips/blob/master/20.md, default[{args["nip33"]}]""",
+                        default=args['nip33'])
+    parser.add_argument('--no-nip33', action='store_false',dest='nip33',
+                        help=f"""disable NIP33 - Parameterized Replaceable Events, default[{not args["nip33"]}]""",
+                        default=args['nip33'])
+
+    # end nips
     parser.add_argument('--ssl', action='store_true', help='run ssl ssl_key and ssl_cert will need to be defined',
                         default=args['ssl'])
     parser.add_argument('--tor', action='store_true', help='make realy accessable over tor',
@@ -193,9 +179,9 @@ def get_args() -> dict:
         'pg_password': PG_PASSWORD,
         'maxsub': MAX_SUB,
         'maxlength': MAX_CONTENT_LENGTH,
-        'nip15': True,
         'nip16': True,
         'nip20': True,
+        'nip33': True,
         'ssl': SSL,
         'ssl_key': None,
         'ssl_cert': None,
@@ -253,9 +239,9 @@ async def main(args):
     max_length = args['maxlength']
 
     # get nip flags
-    nip15 = args['nip15']
     nip16 = args['nip16']
     nip20 = args['nip20']
+    nip33 = args['nip33']
 
     # ssl options
     is_ssl = args['ssl']
@@ -278,7 +264,10 @@ async def main(args):
     # create storage object which is either to sqllite, posgres or transient
     if store == 'sqlite':
         my_store = await get_sql_store(filename=args['dbfile'],
-                                       is_nip16=nip16)
+                                       is_nip16=nip16,
+                                       is_nip33=nip33)
+
+    # Postgres store at the moment ios fucked and needs fixing... shouldn't be too much work
     elif store == 'postgres':
         my_store = get_postgres_store(db_name=args['pg_database'],
                                       user=args['pg_user'],
@@ -288,7 +277,8 @@ async def main(args):
         args['pg_password'] = '***'
 
     elif store == 'transient':
-        my_store = RelayMemoryEventStore(is_nip16=nip16)
+        my_store = RelayMemoryEventStore(is_nip16=nip16,
+                                         is_nip33=nip33)
 
     # just running to empty db
     if is_wipe:
@@ -311,7 +301,6 @@ async def main(args):
     my_relay = Relay(my_store,
                      max_sub=max_sub,
                      accept_req_handler=accept_handlers,
-                     enable_nip15=nip15,
                      ack_events=nip20)
 
     ssl_context = None
@@ -321,18 +310,24 @@ async def main(args):
         ssl_context.load_cert_chain(ssl_cert, ssl_key)
         protocol = 'wss'
 
+    def print_run_info():
+        print(f'running relay at {protocol}://{host}:{port}{end_point} persiting events to store {store}')
+        print(f'{"Event treatment (NIP16)".ljust(40)} {nip16}')
+        print(f'{"Command events (NIP20)".ljust(40)} {nip20}')
+        print(f'{"Parameterized replaceable events (NIP33)".ljust(40)} {nip33}')
+
     # access via tor?
     if enable_tor:
         with TORService(relay_port=port,
                         service_dir=None,
                         password=tor_password) as my_tor:
-            print(f'running relay at {protocol}://{host}:{port}{end_point} persiting events to store {store}')
+            print_run_info()
             await my_relay.start(host=host,
                                  port=port,
                                  end_point=end_point,
                                  ssl_context=ssl_context)
     else:
-        print(f'running relay at {protocol}://{host}:{port}{end_point} persiting events to store {store}')
+        print_run_info()
         await my_relay.start(host=host,
                              port=port,
                              end_point=end_point,
